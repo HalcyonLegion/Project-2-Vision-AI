9e401175f8b7f47e05dd57127f7040d9376bcc0b
Lee Webb <leewerbb55@hotmail.com>
2023-09-13 20:02:22 +0800

    updated console logging

--- functions/getRecipe.js
+++ functions/getRecipe.js
@@ -5,54 +5,48 @@ exports.handler = async function(event, context) {
   const { base64data, googleLensApiKey, openaiApiKey } = body;
 
   // Fetch data from Google Vision API
-  let visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${googleLensApiKey}`;
-  
-  let visionRequestBody = {
-    requests: [
-      {
-        image: { content: base64data.split(",")[1] },
-        features: [
-          { type: "WEB_DETECTION", maxResults: 5 },
-          { type: "LABEL_DETECTION", maxResults: 5 },
-        ],
-      },
-    ],
-  };
+let visionApiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${googleLensApiKey}`;
+const visionRequestBody = {
+  requests: [
+    {
+      image: { content: base64data.split(",")[1] },
+      features: [
+        { type: "WEB_DETECTION", maxResults: 5 },
+        { type: "LABEL_DETECTION", maxResults: 5 },
+      ],
+    },
+  ],
+};
 
-
-  console.log("Sending to Google Lens:", visionRequestBody); // Console Log
-
-  const visionResponse = await fetch(visionApiUrl, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(visionRequestBody),
-  });
+console.log(`Sending the following payload to Google Vision API: ${JSON.stringify(visionRequestBody)}`);
+const visionResponse = await fetch(visionApiUrl, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(visionRequestBody),
+});
 
   const visionData = await visionResponse.json();
   const description = visionData.responses[0].labelAnnotations[0].description;
 
   // Fetch data from OpenAI API
   let openaiUrl = "https://api.openai.com/v1/chat/completions";
+const openaiRequestBody = {
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "system",
+      content: "You are a Recipe and Nutrition expert who finds recipes and nutritional information based on the user's input.",
+    },
+    { role: "user", content: `Please return a recipe based on ${description} and give me some nutritional information about the recipe.` },
+  ],
+};
 
-  let openaiRequestBody = {
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a Recipe and Nutrition expert who finds recipes and nutritional information based on the user's input.",
-      },
-      { role: "user", content: `Please return a recipe based on ${description} and give me some nutritional information about the recipe.` },
-    ],
-  };
-
-  console.log("Sending to OpenAI:", openaiRequestBody); // Console Log
-
-  const openaiResponse = await fetch(openaiUrl, {
-    method: "POST",
-    headers: { "Content-Type": "application/json", Authorization: `Bearer ${openaiApiKey}` },
-    body: JSON.stringify(openaiRequestBody),
-  });
+console.log(`Sending the following payload to OpenAI API: ${JSON.stringify(openaiRequestBody)}`);
+const openaiResponse = await fetch(openaiUrl, {
+  method: "POST",
+  headers: { "Content-Type": "application/json", Authorization: `Bearer ${openaiApiKey}` },
+  body: JSON.stringify(openaiRequestBody),
+});
 
   const openaiData = await openaiResponse.json();
   const recipe = openaiData.choices[0].message.content;
